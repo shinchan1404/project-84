223082388e19dd03ba124a575d85df6e39c8ad87
shinchan1404 <86519253+shinchan1404@users.noreply.github.com>
2021-07-21 23:51:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,81 @@
+
+
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+
+car1_width = 120;
+car1_height = 70;
+car1_image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi-8iTPKFBGzfO9wMLukAyiPI6Crxj4dA2AQ&usqp=CAU";
+car1_x = 10;
+car1_y = 10;
+
+car2_width = 120;
+car2_height = 70;
+car2_image ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTci4KCqZE9pWOJlfI7rU5ndNtN6KRQjUM61Q&usqp=CAU";
+car2_x = 10;
+car2_y = 100;
+
+background_image = "https://m.media-amazon.com/images/I/41dy4YRHIML._AC_SX355_.jpg";
+
+function add() {
+	background_imgTag = new Image(); 
+	background_imgTag.onload = uploadBackground;
+	background_imgTag.src = background_image;   
+
+	car1_imgTag = new Image(); 
+	car1_imgTag.onload = uploadcar1; 
+	car1_imgTag.src = car1_image;   
+
+	car2_imgTag = new Image(); 
+	car2_imgTag.onload = uploadcar2; 
+	car2_imgTag.src = car2_image;   
+}
+
+function uploadBackground() {
+	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+}
+
+function uploadcar1() {
+	ctx.drawImage(car1_imgTag, car1_x, car1_y, car1_width, car1_height);
+}
+function uploadcar2() {
+	ctx.drawImage(car2_imgTag, car2_x, car2_y, car2_width, car2_height);
+}
+
+window.addEventListener("keydown",my_keydown)
+function my_keydown(){
+    keyPressed=e.keyCode;
+    console.log(keyPressed);
+    if(keyPressed=='38')
+    {
+        up();
+    }
+    if(keyPressed=='40')
+    {
+        down()
+    }
+    if(keyPressed=='37')
+    {
+        left()
+    }
+    if(keyPressed=='39')
+    {
+        right()
+    }
+    if(keyPressed=='87')
+    {
+        www()
+    }
+    if(keyPressed=='83')
+    {
+        sss()
+    }
+    if(keyPressed=='65')
+    {
+        aaa()
+    }
+    if(keyPressed=='68')
+    {
+        ddd()
+    }
+}
